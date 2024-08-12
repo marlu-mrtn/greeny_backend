24c7f0fbf23d26e83dc1aacd71d426c5bd7bb1ef
marlu-mrtn <marlu.mrtn@gmail.com>
2024-08-12 19:50:49 +0200

    Initialize project, add README

---
+++ index.js
@@ -0,0 +1,15 @@
+import { createServer } from 'node:http';
+import logger from './app/utils/logger.utils.js';
+import 'dotenv/config';
+
+import app from './app/index.app.js';
+
+const PORT = process.env.PORT || 4000;
+const DBHOST = process.env.DB_HOST || 'localhost';
+
+const httpServer = createServer(app);
+
+httpServer.listen(PORT, () => {
+    logger.info(`Server run on http://${DBHOST}:${PORT}`)
+});
+
